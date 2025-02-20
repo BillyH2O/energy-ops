1ad8c86cb90c3d39ff85c090213f1707d8238de5
Billy <billy.billio.bones@gmail.com>
2025-02-20 10:30:04 +0100

    ✨ Test unitaires

---
+++ tests/pipelines/loading/test_nodes.py
@@ -0,0 +1,8 @@
+# import pandas as pd
+
+from energy_ops.pipelines.loading.nodes import load_csv_from_bucket
+
+
+def test_load_csv_from_bucket(project_id, primary_folder):
+    df = load_csv_from_bucket(project_id, primary_folder)
+    print(df.head())
